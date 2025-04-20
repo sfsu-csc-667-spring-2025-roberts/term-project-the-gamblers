c370c1569ef8db212748b987b89702af0516fe6b
KyleNguyen00 <kyle08ng@gmail.com>
2025-04-20 15:00:09 -0700

    Updating index.js to get my login working

--- src/db/users/index.js
+++ src/db/users/index.js
@@ -1,34 +1,35 @@
-import db from '../connection.js'
-import bcrypt from 'bcryptjs';
+import db from "../connection.js";
+import bcrypt from "bcryptjs";
 
 const register = async (username, email, password) => {
-    const encryptedPassword = await bcrypt.hash(password, 10);
-    console.log(db);
+  const encryptedPassword = await bcrypt.hash(password, 10);
+  console.log(db);
 
-    try {
-        const result = await db.query(
-            "INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id",
-            [username, email, encryptedPassword]
-        );
-        const { id } = result.rows[0];
-        return id;
-    } catch (error) {
-        console.error("Error registering user:", error);
-        throw new Error("User registration failed");
-    }
-};;
-
-const login = async (email, password) => {
-    const result = await db.query("SELECT * FROM users WHERE email = $1", [email]);
-
-    const user = result.rows[0];
-
-    const passwordsMatch = await bcrypt.compare(password, user.password);
-    if (!passwordsMatch) {
-        throw new Error("Failed to Login");
-    }
-    return user.id;
-        
+  try {
+    const result = await db.query(
+      "INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3) RETURNING user_id",
+      [username, email, encryptedPassword],
+    );
+    const { user_id } = result.rows[0];
+    return user_id;
+  } catch (error) {
+    console.error("Error registering user:", error);
+    throw new Error("User registration failed");
+  }
 };
 
-export default {register, login};
+const login = async (email, password) => {
+  const result = await db.query("SELECT * FROM users WHERE email = $1", [
+    email,
+  ]);
+
+  const user = result.rows[0];
+
+  const passwordsMatch = await bcrypt.compare(password, user.password);
+  if (!passwordsMatch) {
+    throw new Error("Failed to Login");
+  }
+  return user.id;
+};
+
+export default { register, login };
